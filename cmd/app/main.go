59eb5de4fee0a449bb42aebaae37c7c763479c68
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-22 01:55:52 -0300

    initialize webserver :fire:

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
 
 	"github.com/Guilherme-Matosoli/go-api/internal/infra/akafka"
 	"github.com/Guilherme-Matosoli/go-api/internal/infra/repository"
+	"github.com/Guilherme-Matosoli/go-api/internal/infra/web"
+	"github.com/Guilherme-Matosoli/go-api/internal/usecase"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -16,9 +21,34 @@ func main() {
 	}
 
 	defer db.Close()
+	repository := repository.NewProductRepositoryPg(db)
+	createProductUsecase := usecase.NewCreateProductUseCase(repository)
+	listProductUsecase := usecase.NewListProductsUseCase(repository)
+
+	productHandlers := web.NewProductHandlers(createProductUsecase, listProductUsecase)
+
+	r := chi.NewRouter()
+
+	r.Post("/products", productHandlers.CreateProductHandler)
+	r.Get("/products", productHandlers.ListProductsHandler)
+
+	go http.ListenAndServe(":4000", r)
 
 	msgChan := make(chan *kafka.Message)
-	akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
+	go akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
 
-	repository := repository.NewProductRepositoryPg(db)
+	for msg := range msgChan {
+		dto := usecase.CreateProductInputDto{}
+		err := json.Unmarshal(msg.Value, &dto)
+
+		if err != nil {
+			println(err)
+		}
+
+		_, err = createProductUsecase.Execute(dto)
+
+		if err != nil {
+
+		}
+	}
 }
